9512a4e4238ef1a116fde604bf9ab070a4a77895
Wesley Zebrick <wesley.zebrick@selu.edu>
2018-04-05 11:45:33 -0500

    Event Binding, onClick,

--- binding/src/app/test/test.component.ts
+++ binding/src/app/test/test.component.ts
@@ -5,38 +5,36 @@ import { Component, OnInit } from '@angular/core';
   template: `
             <h2>
                 Welcome {{name}}
-            <h2 [style.color]= "hasError ? 'red' : 'green'"> Style Binding </h2>
-            <h2 [style.color]= "highlightColor">  Style Binding 2</h2>
-            <h2 [ngStyle]= "titleStyles"> Style Binding 3 </h2>
-            `,
-  styles: [`
+            </h2>
+            <button (click)="onClick($event)" >Greet</button>
+            {{greeting}}
 
-  `]
+            <button (click) = "greeting = 'Welcome Wesley'">Greet</button>
+            `,
+  styles: []
 })
 export class TestComponent implements OnInit {
 
-  public highlightColor = "orange";
-  public hasError = false;
-  public successClass = "text-success";
-  public name = "Wesawoo";
-  public isSpecial = true;
-  public titleStyles = {
-    color: "blue",
-    fontStyle: "italic"
-  }
-  public messageClasses ={
-    "text-success": !this.hasError,
-    "text-danger": this.hasError,
-    "text-special": this.isSpecial
-
-  }
+  public name = "Wesley";
+  public greeting = "";
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  greetUser() {
-    return "Hello " + this.name;
+  onClick(event) {
+    console.log(event);
+    this.greeting = event.ctrlKey;
   }
 }
+
+
+
+
+
+
+
+
+// event binding lets us capture a dom event and perform any action.
+// by using $ event u can get all of the DOM event properties!
